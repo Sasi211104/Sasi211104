11f6fd80f7fea7d7429cf0109b2cac427361de00
Sasi211104 <159163276+Sasi211104@users.noreply.github.com>
2024-02-29 10:02:52 +0530

    Create SimpInheri.cpp

---
+++ SimpInheri.cpp
@@ -0,0 +1,18 @@
+// C++ program to demonstrate the working of public 
+// simple inheritance 
+#include <iostream> 
+using namespace std; 
+
+class superclass { 
+    public:
+        int a = 1;
+        int b= 2;
+};
+class subclass: public superclass{
+    public:
+        int c=3;
+};
+int main(){
+    subclass obj;
+    cout<<(obj.a+obj.b+obj.c)<<endl;
+}
