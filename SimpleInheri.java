85106f8194130fe7de3f59bd3484595b9fc871cf
Sasi211104 <159163276+Sasi211104@users.noreply.github.com>
2024-03-04 22:07:12 +0530

    Create SimpleInheri.java

---
+++ SimpleInheri.java
@@ -0,0 +1,37 @@
+class dog
+{
+    public
+        void name()
+        {
+            System.out.print("Name of the dog is bruno \n ");
+        }
+        void owner()
+        {
+            System.out.print("My owner is pandu\n");
+        }
+}
+
+class puppy extends dog
+{
+    public
+        void play()
+        {
+            System.out.print("he is hyper active \n");
+        }
+        void skills()
+        {
+            System.out.print("He can easily sense the objects\n");
+        }
+}
+
+class SimpleInheritance
+{
+    public static void main(String[] args)
+    {
+        puppy baby=new puppy();
+        baby.name();;
+        baby.owner();
+        baby.play();
+        baby.skills();
+    }
+}
