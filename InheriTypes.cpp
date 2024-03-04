7cc94cbf3289ccc2d3696d0a0039a0d938ed0b59
Sasi211104 <159163276+Sasi211104@users.noreply.github.com>
2024-03-04 21:42:24 +0530

    Update InheriTypes.cpp

--- InheriTypes.cpp
+++ InheriTypes.cpp
@@ -53,6 +53,38 @@ class son3: public dad{
     public:
     string d= "son3";
 };
+//hybrid inheritance
+class A
+{
+ 	public:
+ 	int x;
+};
+class B : public A
+{
+ 	public:
+ 	B()      //constructor to initialize x in base class A
+ 	{
+ 	   x = 10;
+ 	}
+};
+class C
+ {
+ 	public:
+ 	int y;
+ 	C()   //constructor to initialize y
+ 	{
+ 	    y = 4;
+        }
+};
+class D : public B, public C   //D is derived from class B and class C
+{
+ 	public:
+ 	void sum()
+ 	{
+ 	    cout << "Sum= " << x + y;
+ 	}
+};
+
 int main(){
     subclass obj;
     grandchild obj1;
@@ -60,9 +92,11 @@ int main(){
     son1 obj3;
     son2 obj4;
     son3 obj5;
+    D obj6; 
     cout<<"simple inheritance"<<endl<<obj.x+obj.y<<endl;
     cout<<"Multilevel inheritance"<<endl<<obj1.a<<"/"<<obj1.b<<"/"<<obj1.c<<endl;
     cout<<"multiple inheritance"<<endl<<obj2.a1<<endl<<obj2.b2<<endl<<obj2.c1<<endl;
     cout<<"hierarichical inheritance"<<endl<<obj3.a<<endl;
-    cout<<obj3.b<<endl<<obj4.c<<endl<<obj5.d<<endl;
+    cout<<obj3.b<<endl<<obj4.c<<endl<<obj5.d<<endl;     
+ 	obj6.sum();
 }
