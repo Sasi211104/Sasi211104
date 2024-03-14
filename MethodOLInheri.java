6b769d3e48ce0b7a9159ccdd5f4d706410082d2c
Sasi211104 <159163276+Sasi211104@users.noreply.github.com>
2024-03-14 11:43:03 +0530

    Create MethodOLInheri.java

---
+++ MethodOLInheri.java
@@ -0,0 +1,44 @@
+class Student
+{
+    String FullName;
+    int RollNumber;
+    double Cgpa;
+    String Department;
+    void getdata(String name,int roll,double cgpa,String dep)
+    {
+        FullName=name;
+        RollNumber=roll;
+        Cgpa=cgpa;
+        Department=dep;
+    }
+}
+class Student1 extends Student{
+    String Section;
+    void getdata(String name,int roll,double cgpa,String dep,String sec)
+    {
+        FullName=name;
+        RollNumber=roll;
+        Cgpa=cgpa;
+        Department=dep;
+        Section=sec;
+    }
+    void display()
+    {
+        System.out.println("Student Details\n");
+        System.out.println("Name: "+FullName);
+        System.out.println("Roll Number: "+RollNumber);
+        System.out.println("Cgpa: "+Cgpa);
+        System.out.println("Department: "+Department);
+        System.out.println("Section: "+Section);
+    }
+}
+class MethodOLInheri{
+    public static void main(String[] args){
+        Student1 s1 = new Student1();
+        s1.getdata("sasi",516,8.8,"CSE");
+        s1.display();
+        Student1 s2 = new Student1();
+        s2.getdata("sasi",516,8.8,"CSE","A");
+        s2.display();
+    }
+}
